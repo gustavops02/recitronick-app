30c6736c77ea951e936071b1e0a1111d6573530f
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-05-22 12:30:36 -0300

    Initial input search is done.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const Express = require('express');
-const app = Express();
-
-app.listen(PORT, (err)=>{
-    if(err) {
-        throw err;
-        console.log({"Error": "Server is not created."})
-    }
-})
